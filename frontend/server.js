ffeb47a885f8cf6bc5ee77f40efa3713ba6ee0e5
роман сницар <roman.sn@ukr.net>
2025-03-28 23:51:36 +0100

    server frontend added host

--- frontend/server.js
+++ frontend/server.js
@@ -13,6 +13,6 @@ app.get("*", (req, res) => {
 
 // Define port
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Frontend is running on port ${PORT}`);
 });
